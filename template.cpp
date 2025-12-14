c578135b30df8bdb19489e038cb23e722c0cc744
Viriya6 <viriyanand4@gmail.com>
2025-12-14 22:25:41 +0700

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define CILUK ios_base::sync_with_stdio(false);
+#define BAA cin.tie(nullptr)
+#define ll long long
+using namespace std;
+
+int main(){
+    CILUK BAA;
+    return 0;
+}
